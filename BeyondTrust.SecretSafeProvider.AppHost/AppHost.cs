35c010d2e256107cdab6592eea58b825f46ac8a7
Alberto Monteiro <alberto.monteiro@live.com>
2026-03-07 15:28:57 -0300

    Added Aspire

---
+++ BeyondTrust.SecretSafeProvider.AppHost/AppHost.cs
@@ -0,0 +1,22 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var repoRoot = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, ".."));
+var mappingsPath = Path.Combine(Directory.GetCurrentDirectory(), "__admin", "mappings");
+
+var secretsafe = builder
+    .AddWireMock("secretsafe-mock")
+    .AsHttp2Service()
+    .WithMappingsPath(mappingsPath)
+    .WithWatchStaticMappings()
+    .WithOpenTelemetry();
+
+builder.AddDockerfile("terraform", repoRoot, Path.Combine(repoRoot, "Dockerfile.test"))
+    .WaitFor(secretsafe)
+    .WithReference(secretsafe);
+
+//builder.AddProject<Projects.BeyondTrust_SecretSafeProvider>("provider")
+//    .WithHttpsEndpoint(port: 9999, targetPort: 5000, name: "dashboard")
+//    .WaitFor(secretsafe)
+//    .WithReference(secretsafe);
+
+builder.Build().Run();
